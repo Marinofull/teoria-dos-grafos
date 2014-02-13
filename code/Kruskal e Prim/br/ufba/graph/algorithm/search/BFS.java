8cbfcd5c23782787f2c9548715ef29f00c42f720
Marinofull <intmarinoreturn0@gmail.com>
2014-02-13 16:16:21 -0300

    Finalizado BFS e lista fifo

--- code/Kruskal e Prim/br/ufba/graph/algorithm/search/BFS.java
+++ code/Kruskal e Prim/br/ufba/graph/algorithm/search/BFS.java
@@ -6,8 +6,10 @@ import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.Stack;
 
+
 import java.util.Queue;
 import br.ufba.datastructures.AdjacencyMatrix;
+import br.ufba.datastructures.Fifo;
 import br.ufba.graph.Graph;
 import br.ufba.graph.Vertice;
 import br.ufba.graph.Aresta.Status;
@@ -21,7 +23,8 @@ public class BFS implements GraphAlgorithm{
 	private boolean processing = false;
 	private Random gerador = new Random();
 	private int d[];
-	private Vertice v0, v, u; 
+	private Vertice v0, v, u;
+	private Fifo fifo;
 	
 	//private ArrayList<Vertice> Q= new ArrayList<Vertice>();
 	private Queue<Vertice> Q = new PriorityQueue<Vertice>();
@@ -37,6 +40,7 @@ public class BFS implements GraphAlgorithm{
 		matrix = graph.createAdjacencyMatrix();
 		d = new int[graph.getVerticesCount()];//
 		v0 = graph.getVertices()[gerador.nextInt(graph.getVerticesCount())];
+		fifo = new Fifo(graph.getVerticesCount());
 	}
 	
 	
@@ -48,19 +52,24 @@ public class BFS implements GraphAlgorithm{
 	
 	public void buscaemlargura() {
 		int i;
+		boolean buffer;
 		
 		for(i=0; i < graph.getVerticesCount() ; d[i++]= 999);
-		d[v0.index]=0;Q.add(v0);
+		d[v0.index]=0;fifo.add(v0.index);
 		
-		while(!Q.isEmpty()){
-			u=Q.poll();
+		System.out.println("raiz = "+v0.nome);
+		
+		while(!fifo.isEmpty()){
+			u=graph.getVertices()[fifo.remove()];
 			for(i=0; i < graph.getVerticesCount() ; i++){
 				v=graph.getVertices()[i];
 				
 				if(matrix.checkAdjacency(u.index, v.index)){
 					if(d[v.index]==999){
 						d[v.index]=d[u.index]+1;
-						Q.add(v);
+						System.out.println("vertice " + v.nome + " distancia "+d[v.index]+"do vertice raiz");
+						//buffer=Q.add(v);
+						fifo.add(v.index);
 						graph.getAresta(u.index, v.index).status = Status.TAKED;
 					}
 				}
